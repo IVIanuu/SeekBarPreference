e4f09c66366f5eaa8cd138d6ebcb5f257567267b
IVIanuu <IVIanuu@gmail.com>
2018-06-07 16:41:08 +0200

    Target sdk 28

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -2,17 +2,17 @@
 
 object Versions {
     // android
-    const val compileSdk = 27
+    const val compileSdk = 28
     const val minSdk = 14
-    const val targetSdk = 27
+    const val targetSdk = 28
     const val versionCode = 1
     const val versionName = "1.0"
 
     const val kotlin = "1.2.41"
     const val mavenGradle = "2.1"
-    const val support = "27.1.1"
+    const val support = "28.0.0-alpha3"
 }
-//
+
 object Deps {
     const val androidGradlePlugin = "com.android.tools.build:gradle:3.1.2"
 
