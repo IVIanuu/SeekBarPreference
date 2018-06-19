96b0a9f3479912b5105493609b21c532dcba02f7
IVIanuu <IVIanuu@gmail.com>
2018-06-19 15:13:38 +0200

    Added value text provider lambda

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -8,13 +8,13 @@ object Versions {
     const val versionCode = 1
     const val versionName = "1.0"
 
-    const val kotlin = "1.2.41"
+    const val kotlin = "1.2.50"
     const val mavenGradle = "2.1"
     const val support = "28.0.0-alpha3"
 }
 
 object Deps {
-    const val androidGradlePlugin = "com.android.tools.build:gradle:3.1.2"
+    const val androidGradlePlugin = "com.android.tools.build:gradle:3.1.3"
 
     const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
     const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jre7:${Versions.kotlin}"
